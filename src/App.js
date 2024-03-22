0a5116813db0f9a780b9cf4c58f495ab0b9faf01
Amarnath Devshatwar <amarnathdevshatwar.scoe.chem@gmail.com>
2024-03-22 17:19:47 +0530

    1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/heybudday" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
